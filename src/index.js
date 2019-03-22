c0db5491080193812537825032ff61df5cffab1f
Travis Toal <travisty12@gmail.com>
2019-03-22 11:42:12 -0700

    add full output, options to search by symptom or name, refactor doc.ready for maximum concision

--- src/index.js
+++ src/index.js
@@ -2,30 +2,63 @@ import $ from 'jquery';
 import './sass/styles.scss';
 import { DocCall } from './web-doc.js';
 
+function searchCall(input) {
+  $("#input").fadeOut();
+  let searchSucceed = false;
+  const docCall = new DocCall();
+  const promise = docCall.getPromise();
+  promise.then(function(response) {
+    const data = JSON.parse(response).data;
+    const userIn = $("#userIn").val();
+    data.forEach(function(doc) {
+      for (let i = 0; i < doc.specialties.length; i++) {
+        const choice = [doc.specialties[i].description, doc.profile.last_name];
+        if (choice[input].match(userIn)) {
+          searchSucceed = true;
+          const profile = doc.profile;
+          const pic = profile.image_url;
+          const practice = doc.practices[0];
+          const location = practice.visit_address;
+          const newPatients = practice.accepts_new_patients ? "" : "not ";
+          const hasSite = practice.website ? ` or at their website, ${practice.website}` : ``;
+          $("#output").append(`<img src='${pic}'>`);
+          $("#output").append(`<p>Dr. ${profile.first_name} ${profile.last_name}, located at ${location.street}, ${location.city} ${location.state} ${location.zip}</p>`);
+          $("#output").append(`<p>Dr. ${profile.last_name} can be reached at ${practice.phones[0].number}${hasSite}</p>`);
+          $("#output").append(`<p>Dr. ${profile.last_name} is ${newPatients}currently accepting new patients.</p>`);
+          break;
+        }
+      }
+    })
+    if (!searchSucceed) {
+      $("#output").append(`<p>No doctors matched your search for ${userIn}</p>`);
+    }
+  }, function(error) {
+    $("#output").text(`Request failed: ${error.message}`);
+  })
+  return;
+}
+
+function btnToggle(choice) {
+  const options = [`doctor`,`symptom`];
+  $(`#${options[choice]}Label`).fadeOut();
+  $(`#${options[choice]}Btn`).fadeOut();
+  setTimeout(function() {
+    $(`#${options[1-choice]}Btn`).fadeIn();
+    $(`#${options[1-choice]}Label`).fadeIn();
+  }, 400);
+}
+
 $(document).ready(function() {
   $("#symptom").click(function() {
-    $("#doctorLabel").fadeOut();
-    $("#doctorBtn").fadeOut();
-    setTimeout(function() {
-      $("#symptomBtn").fadeIn();
-      $("#symptomLabel").fadeIn();
-    }, 400);
+    btnToggle(0);
   });
   $("#doc").click(function() {
-    $("#symptomLabel").fadeOut();
-    $("#symptomBtn").fadeOut();
-    setTimeout(function() {
-      $("#doctorBtn").fadeIn();
-      $("#doctorLabel").fadeIn();
-    }, 400);
+    btnToggle(1);
   });
   $("#symptomBtn").click(function() {
-    let docCall = new DocCall();
-    let promise = docCall.getPromise();
-    promise.then(function(response) {
-    
-    }, function(error) {
-      $(".output").text(`Request failed: ${error.message}`);
-    })
+  searchCall(0);
+  });
+  $("#doctorBtn").click(function() {
+    searchCall(1);
   });
 });
--- src/web-doc.js
+++ src/web-doc.js
@@ -1,8 +1,9 @@
 export class DocCall {
   getPromise() {
+    var Promise = require('es6-promise').Promise;
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const apiKey = process.env.exports.apiKey;
+      const apiKey = process.env.apiKey;
       let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=0&limit=100&user_key=' + apiKey;
       request.onload = function() {
         if (this.status === 200) {
